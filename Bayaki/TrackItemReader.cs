b3ab43982b0f5d1351ef9a61f4f54383a755cad3
sayrun <sayrun@hotmail.co.jp>
2016-03-21 11:54:36 +0900

    TrackItemをGPX保存にするためにアクセスクラス作成

---
+++ Bayaki/TrackItemReader.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Bayaki
+{
+    class TrackItemReader : IDisposable
+    {
+        private XmlReader _xmlReader;
+
+        public TrackItemReader( string filePath)
+        {
+            _xmlReader = System.Xml.XmlReader.Create(filePath);
+        }
+
+        public TrackItemReader(XmlReader xr)
+        {
+            _xmlReader = xr;
+        }
+
+        public void Dispose()
+        {
+            if( null != _xmlReader)
+            {
+                _xmlReader.Close();
+                _xmlReader = null;
+            }
+        }
+
+        public bykIFv1.TrackItem Read()
+        {
+            bykIFv1.TrackItem result = null;
+            List<bykIFv1.Point> points = new List<bykIFv1.Point>();
+
+            DateTime createTime = DateTime.Now;
+            string name = string.Empty;
+
+            List<DateTime> wayPoints = new List<DateTime>();
+            while (_xmlReader.Read())
+            {
+                switch (_xmlReader.NodeType)
+                {
+                    case System.Xml.XmlNodeType.Element:
+                        if (0 == string.Compare(_xmlReader.Name, "time", true))
+                        {
+                            string sTime = _xmlReader.ReadString();
+                            DateTime dt;
+                            if (DateTime.TryParse(sTime, out dt))
+                            {
+                                createTime = dt;
+                            }
+                        }
+                        else if (0 == string.Compare(_xmlReader.Name, "name", true))
+                        {
+                            name = _xmlReader.ReadString();
+                        }
+                        else if (0 == string.Compare(_xmlReader.Name, "wpt", true))
+                        {
+                            if (!_xmlReader.IsEmptyElement)
+                            {
+                                string lon = _xmlReader.GetAttribute("lon");
+                                string lat = _xmlReader.GetAttribute("lat");
+
+                                string sEle = string.Empty;
+                                string sTime = string.Empty;
+                                string sSpeed = string.Empty;
+
+                                while (_xmlReader.Read())
+                                {
+                                    switch (_xmlReader.NodeType)
+                                    {
+                                        case System.Xml.XmlNodeType.Element:
+                                            if (0 == string.Compare(_xmlReader.Name, "ele", true))
+                                            {
+                                                sEle = _xmlReader.ReadString();
+                                            }
+                                            else if (0 == string.Compare(_xmlReader.Name, "time", true))
+                                            {
+                                                sTime = _xmlReader.ReadString();
+                                            }
+                                            else if (0 == string.Compare(_xmlReader.Name, "speed", true))
+                                            {
+                                                sSpeed = _xmlReader.ReadString();
+                                            }
+                                            else
+                                            {
+                                                if (!_xmlReader.IsEmptyElement)
+                                                {
+                                                    while (_xmlReader.Read())
+                                                        if (_xmlReader.NodeType == System.Xml.XmlNodeType.EndElement) break;
+                                                }
+                                            }
+                                            continue;
+
+                                        case System.Xml.XmlNodeType.EndElement:
+                                            break;
+
+                                        default:
+                                            continue;
+                                    }
+                                    break;
+                                }
+
+                                DateTime dt;
+                                if (DateTime.TryParse(sTime, out dt))
+                                {
+                                    wayPoints.Add(dt);
+                                }
+                            }
+                        }
+                        else if (0 == string.Compare(_xmlReader.Name, "trkpt", true))
+                        {
+                            if (!_xmlReader.IsEmptyElement)
+                            {
+                                string lon = _xmlReader.GetAttribute("lon");
+                                string lat = _xmlReader.GetAttribute("lat");
+
+                                string sEle = string.Empty;
+                                string sTime = string.Empty;
+                                string sSpeed = string.Empty;
+
+                                while (_xmlReader.Read())
+                                {
+                                    switch (_xmlReader.NodeType)
+                                    {
+                                        case System.Xml.XmlNodeType.Element:
+                                            if (0 == string.Compare(_xmlReader.Name, "ele", true))
+                                            {
+                                                sEle = _xmlReader.ReadString();
+                                            }
+                                            else if (0 == string.Compare(_xmlReader.Name, "time", true))
+                                            {
+                                                sTime = _xmlReader.ReadString();
+                                            }
+                                            else if (0 == string.Compare(_xmlReader.Name, "speed", true))
+                                            {
+                                                sSpeed = _xmlReader.ReadString();
+                                            }
+                                            else
+                                            {
+                                                if (!_xmlReader.IsEmptyElement)
+                                                {
+                                                    while (_xmlReader.Read())
+                                                        if (_xmlReader.NodeType == System.Xml.XmlNodeType.EndElement) break;
+                                                }
+                                            }
+                                            continue;
+
+                                        case System.Xml.XmlNodeType.EndElement:
+                                            break;
+
+                                        default:
+                                            continue;
+                                    }
+                                    break;
+                                }
+
+                                DateTime dt;
+                                if (DateTime.TryParse(sTime, out dt))
+                                {
+                                    bool wayPoint = (0 <= wayPoints.IndexOf(dt));
+
+                                    bykIFv1.Point item = new bykIFv1.Point(dt.ToUniversalTime(), decimal.Parse(lat), decimal.Parse(lon), decimal.Parse(sEle), decimal.Parse(sSpeed), wayPoint);
+                                    points.Add(item);
+                                }
+                            }
+                        }
+                        break;
+                }
+            }
+
+            if( 0 >= name.Length)
+            {
+                name = "location data";
+            }
+            // 応答を作成しますよ
+            result = new bykIFv1.TrackItem(name, createTime);
+            result.Items = points;
+
+            return result;
+        }
+    }
+}
---
+++ Bayaki/TrackItemWriter.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Bayaki
+{
+    internal class TrackItemWriter : IDisposable
+    {
+        private XmlWriter _xmlWriter;
+
+        public TrackItemWriter(string filePath)
+        {
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            settings.IndentChars = "\t";
+            settings.Encoding = Encoding.UTF8;
+
+            _xmlWriter = XmlWriter.Create(filePath, settings);
+        }
+
+        internal TrackItemWriter(string filePath, bool indent)
+        {
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = indent;
+            settings.Encoding = Encoding.UTF8;
+
+            _xmlWriter = XmlWriter.Create(filePath, settings);
+        }
+
+
+        public TrackItemWriter(XmlWriter xw)
+        {
+            _xmlWriter = xw;
+        }
+
+        public void Dispose()
+        {
+            if (null != _xmlWriter)
+            {
+                _xmlWriter.Flush();
+                _xmlWriter.Close();
+                _xmlWriter = null;
+            }
+        }
+
+        public void Write(bykIFv1.TrackItem trackItem)
+        {
+            _xmlWriter.WriteStartElement("gpx");
+            {
+                _xmlWriter.WriteAttributeString("version", "1.0");
+                _xmlWriter.WriteAttributeString("creator", "Bayaki");
+
+                _xmlWriter.WriteAttributeString("xmlns", "xsi", null, "http://www.w3.org/2001/XMLSchema-instance");
+                _xmlWriter.WriteAttributeString("schemaLocation", "http://www.w3.org/2001/XMLSchema-instance", "http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd");
+
+                // 名前
+                if (0 < trackItem.Name.Length)
+                {
+                    _xmlWriter.WriteElementString("name", trackItem.Name);
+                }
+
+                // 詳細
+                if (0 < trackItem.Description.Length)
+                {
+                    _xmlWriter.WriteElementString("desc", trackItem.Description);
+                }
+
+                // 作成時間
+                _xmlWriter.WriteElementString("time", trackItem.CreateTime.ToString("yyyy-MM-ddTHH:mm:ssZ"));
+
+                _xmlWriter.WriteStartElement("bounds");
+                {
+                    decimal minlat = trackItem.Items[0].Latitude;
+                    decimal maxlat = trackItem.Items[0].Latitude;
+                    decimal minlon = trackItem.Items[0].Longitude;
+                    decimal maxlon = trackItem.Items[0].Longitude;
+                    foreach (bykIFv1.Point point in trackItem.Items)
+                    {
+                        if (point.Latitude < minlat) minlat = point.Latitude;
+                        if (point.Latitude < maxlat) maxlat = point.Latitude;
+                        if (point.Longitude < minlon) minlon = point.Longitude;
+                        if (point.Longitude > maxlon) maxlon = point.Longitude;
+                    }
+                    _xmlWriter.WriteAttributeString("minlat", minlat.ToString("0.######"));
+                    _xmlWriter.WriteAttributeString("minlon", minlon.ToString("0.######"));
+                    _xmlWriter.WriteAttributeString("maxlat", maxlat.ToString("0.######"));
+                    _xmlWriter.WriteAttributeString("maxlon", maxlon.ToString("0.######"));
+                }
+                _xmlWriter.WriteEndElement();
+
+                // waypointを出力します
+                int index = 0;
+                foreach (bykIFv1.Point point in trackItem.Items)
+                {
+                    ++index;
+                    if (point.Interest)
+                    {
+                        _xmlWriter.WriteStartElement("wpt");
+                        {
+                            _xmlWriter.WriteAttributeString("lat", point.Latitude.ToString("0.######"));
+                            _xmlWriter.WriteAttributeString("lon", point.Longitude.ToString("0.######"));
+
+                            _xmlWriter.WriteElementString("ele", point.Elevation.ToString());
+                            _xmlWriter.WriteElementString("time", point.Time.ToString("yyyy-MM-ddTHH:mm:ssZ"));
+                            _xmlWriter.WriteElementString("speed", point.Speed.ToString("0.######"));
+                            _xmlWriter.WriteElementString("name", string.Format("PT{0:D4}", index));
+                        }
+                        _xmlWriter.WriteEndElement();
+                    }
+                }
+
+                _xmlWriter.WriteStartElement("trk");
+                {
+                    _xmlWriter.WriteStartElement("trkseg");
+                    {
+                        index = 0;
+                        foreach (bykIFv1.Point point in trackItem.Items)
+                        {
+                            _xmlWriter.WriteStartElement("trkpt");
+                            {
+                                _xmlWriter.WriteAttributeString("lat", point.Latitude.ToString("0.######"));
+                                _xmlWriter.WriteAttributeString("lon", point.Longitude.ToString("0.######"));
+
+                                _xmlWriter.WriteElementString("ele", point.Elevation.ToString());
+                                _xmlWriter.WriteElementString("time", point.Time.ToString("yyyy-MM-ddTHH:mm:ssZ"));
+                                _xmlWriter.WriteElementString("speed", point.Speed.ToString("0.######"));
+                                _xmlWriter.WriteElementString("name", string.Format("PT{0:D4}", ++index));
+                            }
+                            _xmlWriter.WriteEndElement();
+                        }
+                    }
+                    _xmlWriter.WriteEndElement();
+                }
+                _xmlWriter.WriteEndElement();
+            }
+            _xmlWriter.WriteEndElement();
+        }
+    }
+}
